783760ece88ff204fd9be85604b02f07260719cd
Pop Plays <85472155+popQA17@users.noreply.github.com>
2022-04-23 12:51:09 +0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -16,7 +16,7 @@ function MyApp({ Component, pageProps }) {
         <link rel="shortcut icon" type="image/png" href="https://i.imgur.com/eSgi8jm.png" />
         <meta name="google-site-verification" content="7gLQjji2gsG8jRarJfqdouJwnovmJCLM-Kbnv4CVLxw" />
         <meta name="description" content="Improve your online presence with Somelist. Advertise, Explore, and Interact with Discord Bots on this Vast, and Stunning Discord Bot List." />
-        <meta name="twitter:image" content="https://i.imgur.com/KYL5iuC.png" />
+        <meta name="twitter:image" content="https://media.discordapp.net/attachments/895953876296040468/967286226195472404/0D5BA416-09FF-4164-8C97-591AABE3BD85.png" />
     </Head>
     <NextNProgress color='var(--700)'/>
     <Component {...pageProps} />
