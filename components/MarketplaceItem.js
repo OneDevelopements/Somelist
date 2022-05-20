2349b85110abbbe732a775b94215a69f0083319d
Pop Plays <85472155+popQA17@users.noreply.github.com>
2022-05-20 21:46:43 +0800

    e

---
+++ components/MarketplaceItem.js
@@ -0,0 +1,129 @@
+import { Dialog, Transition } from '@headlessui/react'
+import { Fragment, useState } from 'react'
+import $ from 'jquery'
+import Cookie from 'js-cookie'
+const MarketplaceItem = (props) =>{
+    const [toast, settoast] = useState(false)
+    let [isOpen, setIsOpen] = useState(false)
+    const [error, seterror] = useState(false)
+    function closeModal() {
+        setIsOpen(false)
+      }
+    
+      function openModal() {
+        setIsOpen(true)
+    }
+    return(
+        <>
+        <div className='items-center p-10 bg-blue-900/10 rounded-xl flex'>
+                <div className='rounded-xl flex items-center justify-center p-4 w-24 h-20 min-w-20 min-h-20 bg-yellow-600/30 mr-4'>
+                    <i className='fas fa-crown text-3xl text-yellow-500'></i>
+                </div>
+                <div className='flex flex-col w-full'>
+                    <h1 className='text-xl'>{props.name}</h1>
+                    <p className='text-gray-100/50 '>i{props.description}</p>
+                </div>
+                <div>
+                    <button disabled={props.owned} onClick={() => openModal()} className='disabled:bg-gray-700 hover:bg-violet-600 delay-150 transition-all ease-in-out duration-300 hover:scale-110 hover:shadow-xl shadow-violet-100 ring-0 shadow-none p-4 rounded-xl bg-blue-600 px-10 ml-auto'>
+                    {props.owned ? 'Owned' : 'Buy'}
+                    </button>
+                </div>
+        </div>
+        <Transition appear show={isOpen} as={Fragment}>
+            <Dialog as="div" className="relative z-10" onClose={closeModal}>
+            <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0"
+                enterTo="opacity-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0"
+            >
+                <div className="fixed inset-0 bg-black bg-opacity-25" />
+            </Transition.Child>
+
+            <div className="fixed inset-0 overflow-y-auto">
+                <div className="flex min-h-full items-center justify-center p-4 text-center">
+                <Transition.Child
+                    as={Fragment}
+                    enter="ease-out duration-300"
+                    enterFrom="opacity-0 scale-95"
+                    enterTo="opacity-100 scale-100"
+                    leave="ease-in duration-200"
+                    leaveFrom="opacity-100 scale-100"
+                    leaveTo="opacity-0 scale-95"
+                >
+                    <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-black p-6 text-left align-middle shadow-xl transition-all">
+                    <Dialog.Title
+                        as="h3"
+                        className="text-lg font-medium leading-6 text-gray-100"
+                    >
+                        Are you sure?
+                    </Dialog.Title>
+                    <div className="mt-2">
+                        <p className="text-sm text-gray-100/70">
+                            <b>{props.price} points</b> will be deducted from your account. This action is non-refundable!
+                        </p>
+                    </div>
+
+                    <div className="mt-4 flex w-full">
+                        <button
+                            type="button"
+                            className="ml-auto inline-flex justify-center rounded-md border border-transparent bg-blue-500/20 px-4 py-2 text-sm font-medium text-white hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                            onClick={closeModal}
+                        >
+                            Nevermind
+                        </button>
+                        <button
+                            type="button"
+                            
+                            className="ml-4 inline-flex justify-center rounded-md border border-transparent bg-blue-500 px-4 py-2 text-sm font-medium text-white hover:bg-blue-600 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                            onClick={() => {
+                                    settoast(false)
+                                    $.ajax({
+                                        url: 'https://api.somelist.tk/points/purchase?item='+ props.id + '&price='+props.price+'&token='+Cookie.get('token')
+                                    }).then((res)=>{
+                                        if(res.result == 'TOKEN_INVALID'){
+                                            settoast('Token invalid. Please log in again.')
+                                            seterror(true)
+                                        } else if (res.result == 'NO_FUNDS'){
+                                            settoast('You don\'t have the points to buy this asset.')
+                                            seterror(true)
+                                        } else if (res.result == 'BOUGHT'){
+                                            settoast('You already bought this item. No charges were made.')
+                                            seterror(true)
+                                        } else {
+                                            seterror(false)
+                                            settoast('Purchase succeeded. Reloading for changes.')
+                                            setTimeout(()=>{
+                                                window.location.reload()
+                                            }, 1500)
+                                        }
+                                        closeModal()
+                                        setTimeout(()=>{
+                                            settoast(false)
+                                        }, 3000)
+                                    })
+                                }
+                            }
+                        >
+                        Purchase
+                        </button>
+                    </div>
+                    </Dialog.Panel>
+                </Transition.Child>
+                </div>
+            </div>
+            </Dialog>
+        </Transition>
+        {toast && 
+            <div style={{zIndex: 1000}} className={`${error ? 'bg-red-500' : 'bg-green-500'} p-4 text-center fixed left-0 top-0 w-screen`}>
+                {toast}
+            </div>
+        }
+        </>
+
+    )
+}
+export default MarketplaceItem
---
+++ pages/store.js
@@ -0,0 +1,49 @@
+import MarketplaceItem from "../components/MarketplaceItem"
+import HeaderB from "../components/Navbar"
+export default function Store({points, items, isLoggedIn}){
+    return(
+    <>
+    <HeaderB isLoggedIn={isLoggedIn}/>
+    <div className='p-5 lg:p-10 py-[8rem] lg:py-[8rem] rounded-lg min-h-screen	'>
+        <div className='mb-10 flex'>
+            <h1 className='text-4xl text-blue-500 font-bold'>The Marketplace</h1>
+            <div className='ml-auto p-6 rounded-xl bg-blue-700 py-4'>
+                {points} points
+            </div>
+        </div>
+        <div className='mb-20'>
+        <input
+        placeholder='Search for a marketplace item...'
+        className="transition-all duration-200 my-2 w-full bg-black/10 dark:bg-black/30 dark:hover:bg-black/50 hover:bg-black/20 text-dark dark:text-white focus:text-sky-500 rounded-xl border border-white/10 focus:border-sky-500 outline-none py-4 px-6"/>
+        </div>
+        <div>
+            {items.map((item)=>{
+                return <MarketplaceItem id={item.id} name={item.name} price={item.price} description={item.description} owned={item.owned}/>
+            })}
+        </div>
+    </div>
+    </>
+    )
+}
+
+
+export async function getServerSideProps(context) {
+    if (!context.req.cookies.token){
+        return {
+            redirect: {
+              permanent: false,
+              destination: "https://api.somelist.tk/login",
+            },
+        }
+    }
+    const res = await fetch('https://api.somelist.tk/points/'+ context.req.cookies.id+'?token='+context.req.cookies.token)
+    const json = await res.json()
+    const res2 = await fetch('https://api.somelist.tk/store?token='+context.req.cookies.token)
+    const json2 = await res2.json()
+    return {props: {
+      points: json.points,
+      items: json2.items,
+      isLoggedIn: context.req.cookies.token ? true : false
+    }}
+}
+
