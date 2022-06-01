e3a1c866e7ec331c87c6bace0720e782333312c6
Pop Plays <85472155+popQA17@users.noreply.github.com>
2022-06-01 16:41:03 +0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -67,7 +67,7 @@ function MyApp({ Component, pageProps }) {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
         <link rel="shortcut icon" type="image/png" href="https://i.imgur.com/eSgi8jm.png" />
         <meta name="google-site-verification" content="7gLQjji2gsG8jRarJfqdouJwnovmJCLM-Kbnv4CVLxw" />
-        <meta name="description" content="Improve your online presence with Somelist. Advertise, Explore, and Interact with Discord Bots on this Vast, and Stunning Discord Bot List." />
+        <meta name="description" content="Explore dozens of different Discord Bots on Somelist." />
         <meta name="twitter:image" content="https://media.discordapp.net/attachments/895953876296040468/967286226195472404/0D5BA416-09FF-4164-8C97-591AABE3BD85.png" />
     </Head>
     <NextNProgress color='var(--700)'/>
