701f9e2991bb7550f8cdd57ce71a382bb2ee28ff
Pop Plays <85472155+popQA17@users.noreply.github.com>
2022-06-07 07:48:02 +0800

    Create isScrolledIntoView.js

---
+++ components/isScrolledIntoView.js
@@ -0,0 +1,10 @@
+export default function isScrolledIntoView(elem)
+{
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
+
+    var elemTop = $(elem).offset().top;
+    var elemBottom = elemTop + $(elem).height();
+
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+}
